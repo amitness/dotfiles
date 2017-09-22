a7fa7fccc610602057b83a9109e20d5a688d6e84
Amit <meamitkc@gmail.com>
2017-09-22 21:26:41 +0545

    Remove trailing line

--- scripts/.sublime2y.sh
+++ scripts/.sublime2y.sh
@@ -2,5 +2,4 @@
 qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.addSession; 
 qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.runCommand  "cd $1"  >/dev/null 2>&1 &
 qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.runCommand  "clear" 
-qdbus org.kde.yakuake /yakuake/window toggleWindowState; 
-
+qdbus org.kde.yakuake /yakuake/window toggleWindowState;
