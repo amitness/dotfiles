7997e0138687c19cf9bff41919910327cb15096e
Amit Chaudhary <meamitkc@gmail.com>
2017-12-22 22:05:53 +0545

    Install sublime

--- setup.sh
+++ setup.sh
@@ -52,6 +52,11 @@ sudo apt install -y npm
 npm install -g eslint
 npm install -g csslint
 
+# Install sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update && sudo apt install sublime-text
+
 # Remove default apps
 sudo apt-get -y remove thunderbird
 sudo apt-get -y remove pidgin
