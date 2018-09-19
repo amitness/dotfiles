1515dbd17a98088603a6f3e8c5d662457e9b97df
Amit Chaudhary <meamitkc@gmail.com>
2018-09-19 22:40:47 +0545

    Add scikitlearn missing package

--- setup.sh
+++ setup.sh
@@ -55,6 +55,7 @@ pip install --user scikit-learn
 pip install --user scikit-image
 pip install --user imageio
 pip install --user seaborn
+pip install --user sklearn
 
 # Useful
 sudo apt-get install -y redis-server
