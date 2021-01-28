d45f2bb9e48773c3630fe9864025edb9bf533a96
Amit Chaudhary <meamitkc@gmail.com>
2021-01-28 13:39:14 +0545

    Remove sublime to yakuake script

--- scripts/.sublime2y.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.addSession; 
-qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.runCommand  "cd $1"  >/dev/null 2>&1 &
-qdbus org.kde.yakuake /yakuake/sessions org.kde.yakuake.runCommand  "clear" 
-qdbus org.kde.yakuake /yakuake/window toggleWindowState;
